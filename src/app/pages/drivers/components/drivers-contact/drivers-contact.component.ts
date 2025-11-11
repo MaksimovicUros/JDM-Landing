f7e752c50c8ad5492641f289f0f9004b1975840f
uros <83064029+MaksimovicUros@users.noreply.github.com>
2025-11-11 21:50:22 +0100

    drivers contact form - add agree to terms checkbox

--- src/app/pages/drivers/components/drivers-contact/drivers-contact.component.ts
+++ src/app/pages/drivers/components/drivers-contact/drivers-contact.component.ts
@@ -1,16 +1,22 @@
 import { Component, inject } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { DriversContactService } from './drivers-contact.service';
 
 @Component({
   selector: 'app-drivers-contact',
   templateUrl: './drivers-contact.component.html',
-  styleUrls: ['./drivers-contact.component.scss']
+  styleUrls: ['./drivers-contact.component.scss'],
 })
 export class DriversContactComponent {
   contactService = inject(DriversContactService);
 
   form: FormGroup;
+  acceptTermsControl = new FormControl(false);
 
   positions = [
     'Rent to Run',
@@ -35,10 +41,12 @@ export class DriversContactComponent {
 
   ngAfterViewInit() {
     // fix an autoplay policy issue.
-    const video = document.getElementById('contact-drivers-video') as HTMLVideoElement;
+    const video = document.getElementById(
+      'contact-drivers-video'
+    ) as HTMLVideoElement;
     if (video) {
       video.muted = true;
-      video.play().catch(err => {
+      video.play().catch((err) => {
         console.log('Autoplay blocked:', err);
       });
     }
@@ -53,7 +61,7 @@ export class DriversContactComponent {
 
       this.contactService.submitContact(formData).subscribe({
         next: () => alert('Form sent successfully!'),
-        error: () => alert('Error sending form.'),
+        error: (err) => alert('Error sending form.'),
       });
     }
   }
